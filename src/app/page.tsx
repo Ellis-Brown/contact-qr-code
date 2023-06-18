75a842b9df8b46cf6e11be0b88199c5caa799a1a
Ellis Brown <ellis.brown@tufts.edu>
2023-06-18 14:32:49 -0400

    Fix download, fix title of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 'use client';
 import React, { useState } from "react";
 import QRCode from "react-qr-code";
+import { saveAs } from 'file-saver';
 import VCard from "vcard-creator";
+import { AiFillGithub } from 'react-icons/ai';
+import { toPng } from 'html-to-image';
 
-const QRCodePage: React.FC = () => {
+
+export default function MainPage() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setlastName] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [website, setWebsite] = useState("");
-  const [qrCode, setQrCode] = useState("");
-  const qrCodeRef = React.useRef<HTMLCanvasElement>(null);
+  const ref = React.useRef(null);
+
 
 
   const generateQRCode = () => {
-    
     const vCard = new VCard()
       .addName(lastName, firstName)
       .addEmail(email)
@@ -24,22 +27,25 @@ const QRCodePage: React.FC = () => {
       .addURL(website);
 
     return vCard.toString();
-
   };
 
   const handleDownload = () => {
-    if (qrCodeRef.current) {
-      const qrCodeCanvas = qrCodeRef.current;
-      const qrCodeImage = qrCodeCanvas.toDataURL("image/png");
-      const downloadLink = document.createElement("a");
-      downloadLink.href = qrCodeImage;
-      downloadLink.download = "qrcode.png";
-      downloadLink.click();
+    console.log('here');
+    const qrCodeValue = generateQRCode();
+    const canvas = ref?.current;
+    if (canvas === null) {
+      alert('500 server error');
+      return;
     }
-  };
+    toPng(canvas)
+      .then(function (blob: any) {
+        saveAs(blob, 'contact-qr-code.png');
+      });
+  }
 
   return (
-    <div className="bg-gradient-to-r from-green-500 to-blue-500 min-h-screen flex items-center justify-center">
+
+    <div className="bg-gradient-to-r from-green-500 to-blue-500 min-h-screen flex items-center justify-center flex-col">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg text-white">
         <h1 className="text-3xl mb-6 font-bold text-center bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-transparent">
           Share your contact with a QR code
@@ -117,26 +123,31 @@ const QRCodePage: React.FC = () => {
               className="w-full bg-gray-700 text-white px-3 py-2 rounded focus:outline-none"
             />
           </div>
-          {/* center the below line */}
         </form>
         <div className="text-center">
           <h2 className="text-2xl mb-2 font-bold">QR Code:</h2>
-          <div
-            className="bg-gradient-to-r from-purple-500 to-blue-500 w-60 h-60 rounded-lg mx-auto flex items-center justify-center"
-          >
-            <QRCode value={generateQRCode()} size={220} />
+          <div className="bg-gradient-to-r from-purple-500 to-blue-500 w-60 h-60 rounded-lg mx-auto flex items-center justify-center selectMeClassName">
+            <QRCode ref={ref} value={generateQRCode()} size={220} id={'canvas'} />
           </div>
           <button
             className="mt-4 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             onClick={handleDownload}
+
           >
             Download QR Code
           </button>
         </div>
-        
       </div>
+
+      <a
+        href={'https://github.com/Ellis-Brown/contact-qr-code'}
+        className="text-gray-200 rounded-lg bg-gradient-to-r from-black to-gray-500 hover:bg-gray-600 px-4 py-2 my-4"
+      >
+        <div className="flex flex-row">
+          <AiFillGithub size={30} />&nbsp; Star on GitHub
+        </div>
+      </a>
     </div>
+
   );
 };
-
-export default QRCodePage;
